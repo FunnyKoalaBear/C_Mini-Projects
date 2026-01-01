16e2ca09ef68fd912cfa0d6cb6227ec32d985a13
FunnyKoalaBear <sbmithun@gmail.com>
2026-01-01 14:41:54 +0900

    assignment complete.

--- 6/README.md
+++ 6/README.md
@@ -1,2 +1,18 @@
 Final project program of week 13
 This program just adds vector and matrix multiplication functionality to project folder 5. 
+
+
+6.2 Just for fun
+
+If you consider the vector V to represent the (2-dimensional) point (3,4) then, based on the results of
+
+the multiplications, can you guess what the matrices S and T might represent? If you can then test
+
+your theory by modifying the vector and matrices to see if the results are what you expect.
+
+Can you nowguess whatthe matrix product T ×S =TS represents when applied to a vector V? If
+
+you have a theory about this then test it by modifying S and T and comparing the product TS×V to
+
+your predicted result.
+
--- 6/main.c
+++ 6/main.c
@@ -5,6 +5,10 @@ typedef struct Mat3 {
     double m[3][3];
 } Mat3;
 
+typedef struct Vec3 {
+    double v[3];
+} Vec3; 
+
 
 void Mat3_print_dynamic(const Mat3* M) {
 
@@ -32,6 +36,20 @@ void Mat3_print_dynamic(const Mat3* M) {
 
 }
 
+// new vector print func
+void Vec3_print(const Vec3* V) {
+
+    printf("{");
+
+    for (int i = 0; i < 2; i++) {
+
+        printf("%.6f, ", V->v[i]);
+
+    }
+
+    printf("%.6f } \n", V->v[2]); //final element in V 
+}
+
 void Mat3_mulMat3(const Mat3* A, const Mat3* B, Mat3* P) {
 
     for (int row = 0; row < 3; row++) {
@@ -50,15 +68,64 @@ void Mat3_mulMat3(const Mat3* A, const Mat3* B, Mat3* P) {
     }
 }
 
+//new vector matrix multiplication func
+void Mat3_mulVec3(const Mat3* M, const Vec3* V, Vec3* P) {
+
+    //M X V, need to transpose V, s go by rows 
+    for (int row = 0; row < 3; row++) {
+        
+        P->v[row] = 0; //initialising
+
+        for (int aCol =0; aCol < 3; aCol++) {
+
+            P->v[row] += V->v[aCol] * M->m[row][aCol];
+
+        }
+
+    }
+
+}
+
+
 int main(void){
 
-    //double braces for struct, then array initialiser 
-    Mat3 A = {{{ 2,  1,  5}, { 2, 10,  5}, { 3,  1,  4} }};
-    Mat3 B = {{ { 8,  7,  1}, { 4,  2,  7}, { 2,  3,  5} }};
-    Mat3 P; 
+    Vec3 V = {3, 4 ,1};
+    Vec3_print(&V);
 
-    Mat3_mulMat3(&A, &B, &P);
-    Mat3_print_dynamic(&P);
+
+    Mat3 S = {{ {3, 0, 0}, {0, 2, 0}, {0, 0, 1} }};
+    Vec3 SV; 
+    Mat3_mulVec3(&S, &V, &SV);
+    Vec3_print(&SV);
+
+
+    Mat3 T = {{ {1, 0, 5,}, {0, 1, 7}, {0, 0, 1} }};
+    Vec3 TV;
+    Mat3_mulVec3(&T, &V, &TV);
+    Vec3_print(&TV);
+
+
+    Mat3 TS;
+    Mat3_mulMat3(&T, &S, &TS);
+    Vec3 TSV;
+    Mat3_mulVec3(&TS, &V, &TSV);
+    Vec3_print(&TSV);
+
+    
+    //return 0;
+
+    //the just for fun part 
+    Vec3 V_point = { 3, 4, 1 }; 
+
+    // Theory: If we change S to scale by 2 on X and 2 on Y, 
+    // the point (3,4) should become (6,8).
+    Mat3 S_test = {{ { 2, 0, 0 }, { 0, 2, 0 }, { 0, 0, 1 } }};
+    Vec3 V_scaled;
+    Mat3_mulVec3(&S_test, &V_point, &V_scaled);
+    
+    printf("Result: ");
+    Vec3_print(&V_scaled); // You should see { 6.0, 8.0, 1.0 }
+    printf("\n");
 
     return 0;
 }
